04e9d9e1eddea114f0a4136cdfaf1a16003cae80
Ramazan Kurbanov <ramazan_kurbanov@gap.com>
2017-09-03 13:49:27 -0400

    reading data from mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
 import data from '../src/testData.json'
+import { MongoClient } from 'mongodb'
+import assert from 'assert'
+import config from '../config'
+
+let mdb
+MongoClient.connect(config.mongodbUri, (err, db) => {
+    assert.equal(null, err)
+    console.log('Connected to MongoDB')
+
+    mdb = db
+})
 
 const router = express.Router()
 const contests = data.contests.reduce((contests, contest) => {
@@ -8,14 +19,29 @@ const contests = data.contests.reduce((contests, contest) => {
 }, {})
 
 router.get('/contests', (req, res) => {
-    res.send({ contests })
+    let contests = {}
+    mdb.collection('contests').find({})
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1
+        })
+        .each((err, contest) => {
+            assert.equal(null, err)
+
+            if (!contest) {
+                res.send({contests})
+                return
+            }
+
+            contests[contest.id] = contest
+        })
 })
 
 router.get('/contests/:contestId', (req, res) => {
-    let contest = contests[req.params.contestId]
-    contest.description= 'lorem impsum'
-
-    res.send(contest)
+    mdb.collection('contests').findOne({id: Number(req.params.contestId)})
+        .then(contest => res.send(contest))
+        .catch(console.err)
 })
 
 export default router
